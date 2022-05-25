f763a728571b352e01b31ec413e60584e021175f
Ayuukina <60597271+Ayuukina@users.noreply.github.com>
2022-05-25 16:24:31 +0200

    Ajout a la base de donnée de la création d'un trajet conducteur

---
+++ html/ActionAjout.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Action Ajout</title>
+</head>
+
+<body>
+
+<h1>Ton trajet a bien été mit en ligne</h1>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$db="briquesrouges";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$db);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+$_POST["Description"]= str_replace("'","''",$_POST["Description"]);
+if($_POST["tel"]==1){$_POST["tel"]=true;}
+else{$_POST["tel"]=true;}
+if($_POST["AllerRetour"]=="Aller"){
+    $request="INSERT INTO trajet(TypeTrajet,LieuDépart, DateDépart, HeureDépart, DateAjout, NbPassagers, Prix, Description, DisplayTel,  AnnéeEdition, IdCompte, IdTrajet) VALUES ('".$_POST["AllerRetour"]."','".$_POST["Ville"]."','".$_POST["Date-de-Depart"]."','".$_POST["Heure-de-Depart"]."','".date("d.m.y")."','".$_POST["NbPass"]."','".$_POST["Prix"]."','".$_POST["Description"]."','".$_POST["tel"]."' ,'2022','0','0')";
+}
+else{
+    $request="INSERT INTO trajet(TypeTrajet,LieuArrivée, DateDépart, HeureDépart, DateAjout, NbPassagers, Prix, Description, DisplayTel,  AnnéeEdition, IdCompte, IdTrajet) VALUES ('".$_POST["AllerRetour"]."','".$_POST["Ville"]."','".$_POST["Date-de-Depart"]."','".$_POST["Heure-de-Depart"]."','".date("d.m.y")."','".$_POST["NbPass"]."','".$_POST["Prix"]."','".$_POST["Description"]."','".$_POST["tel"]."' ,'2022','0','0')";
+}
+if ($conn->query($request) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+?>
+</body>
+</html>
--- html/AjoutConducteur.php
+++ html/AjoutConducteur.php
@@ -9,17 +9,19 @@
 <body>
 
     <h1>Ajout d'un trajet conducteur</h1>
-    <form action="">
+    <form action="ActionAjout.php" method="post">
         <div>
             <input type="radio" value="Aller" id="Aller" name="AllerRetour" ><label for="Aller">Aller</label>
             <input type="radio" name="AllerRetour" value="Retour" id="Retour"><label for="Retour">Retour</label>
         </div>
         <div>
             <p>Ville de départ/arrivée:</p>   <input type="text" name="Ville" id="Ville">
-            <p>Adresse de départ/arrivée:</p>   <input type="text" name="Adresse0" id="Adresse">
+            <!--<p>Adresse de départ/arrivée:</p>   <input type="text" name="Adresse0" id="Adresse">-->
         </div>
-        <p>Date et heure de départ:</p>
-        <input type="datetime-local" id="Heure-de-Depart" name="Heure-de-Depart" value="" min="2022-09-16T00h00" max="2022-10-01T23h59">
+        <p>Date de départ:</p>
+        <input type="date" id="Date-de-Depart" name="Date-de-Depart" value="" min="2022-09-16">
+        <p>Heure de départ:</p>
+        <input type="time" id="Heure-de-Depart" name="Heure-de-Depart" value="">
         <p>Date et heure d'arrivée:</p>
         <!--//insérer la date via JS/API google-->
         <br/>
@@ -37,7 +39,7 @@
         <input type="checkbox" id="tel" value="tel" name="tel"> 
         <label>Coche pour rendre ton numéro de téléphone visible sur ton annonce</label>
         <br/>
-        <input type="button" name="send" id="send" value="Envoyer">
+        <input type="submit" name="send" id="send" value="Envoyer">
         <div id="map"></div>
 
 
