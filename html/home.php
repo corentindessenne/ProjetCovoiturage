9373f5aae454a41d019c1c520b41a4f8b0bc587c
Adrien Mareel <56940107+AdriMareel@users.noreply.github.com>
2022-06-02 17:10:29 +0200

    bug double nav

--- html/home.php
+++ html/home.php
@@ -13,17 +13,6 @@
 
 </head>
 <body>
-	<nav>
-		<div class="logo">
-			<a href="home.php"><img src="../images/LBR Ressources/logoLONGUEURClassic.png"></a>
-		</div>
-
-		<ul class="menu">
-			<li class="menu-item"><a href="">Les trajets</a></li>
-			<li class="menu-item connect "><a href="register.php">S'inscrire</a></li>
-			<li class="menu-item connect"><a href="login.php">Se connecter</a></li>
-		</ul>
-	</nav>
 
 	<?php 
 		include 'NavBar.php';
