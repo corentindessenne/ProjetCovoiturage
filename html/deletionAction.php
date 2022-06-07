0574a0369891cb43e3d174bca07846c721db667b
Ayuukina <60597271+Ayuukina@users.noreply.github.com>
2022-06-07 18:32:54 +0200

    delete trajet avec compte

--- html/deletionAction.php
+++ html/deletionAction.php
@@ -2,12 +2,13 @@
 include 'Connexion.php';
 
 $mail=$_SESSION["mail"];
-$sql = "SELECT motDePasse FROM compte WHERE Email='".$mail."'" ;
+$sql = "SELECT motDePasse,IdCompte FROM compte WHERE Email='".$mail."'" ;
 $result = $conn->query($sql);
 if ($result->num_rows >  0) {
     // output data of each row
     $row = $result->fetch_assoc();
       $hashedpassword=$row["motDePasse"];
+      $idcompte=$row["IdCompte"];
       //echo $hashedpassword;
   }
   else{
@@ -20,30 +21,33 @@ if ($result->num_rows >  0) {
           die();
   }
 
-  if(password_verify($_POST["password_1"],$hashedpassword)){
+if(password_verify($_POST["password_1"],$hashedpassword)){
     
     $sql = "DELETE FROM compte WHERE Email='".$mail."'" ;
     if ($conn->query($sql) === TRUE) {
-        session_destroy();
-        ?>
-        <script type="text/javascript">
-            alert("Ton compte a bien été supprimé");
-            location="home.php";
-        </script>
-      <?php
-      die();
-        } else {
-
+        $sql = "DELETE FROM trajet WHERE IdCompte='".$idcompte."'" ;
+        if ($conn->query($sql) === TRUE) {
+            
+            session_destroy();
             ?>
             <script type="text/javascript">
-                alert("Une erreur inatendue c'est produite réessaye plus tard");
+                alert("Ton compte a bien été supprimé");
                 location="home.php";
             </script>
-          <?php
-          die();
-        }
-      
-  }
+            <?php
+            die();
+        } else {
+
+                ?>
+                <script type="text/javascript">
+                    alert("Une erreur inatendue c'est produite réessaye plus tard 2");
+                    location="home.php";
+                </script>
+            <?php
+            die();
+            }
+    }
+}
   else{
         ?>
         <script type="text/javascript">
