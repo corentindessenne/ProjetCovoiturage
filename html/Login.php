ce77500d9e69cc26642511d98e39c7dc1bf9d98b
Corentin DESSENNE <corentin.dessenne@student.junia.com>
2022-06-15 14:23:16 +0200

    ajout bouton google + style

--- html/Login.php
+++ html/Login.php
@@ -1,46 +1,45 @@
-<html>
-    <head>
-       <meta charset="utf-8">
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="google" content="notranslate">
     <title>LBR Covoiturage</title>
     <!-- CSS only -->
     <!--Google Fonts-->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
-        <!-- importer le fichier de style -->
-    <link rel="stylesheet" href="../css/login.css" media="screen" type="text/css" />
-    </head>
-    <body>
-        <div id="container">
-            <!-- zone de connexion -->
-            
-            <form action="LoginAction.php" method="POST">
+    <!-- importer le fichier de style -->
+    <link rel="stylesheet" href="../css/login.css" media="screen" type="text/css"/>
+</head>
+<body>
+<div id="container">
+    <!-- zone de connexion -->
 
-                    <div class="logo">
-                        <a href=""><img src="../images/LBR Ressources/logoLONGUEURClassic.png"></a>
-                    </div>
-                    <h1>Connexion</h1>
-                    <label><b>Email</b></label>
-                    <input type="text" required="required" placeholder="Email" name="email" required>
+    <form action="LoginAction.php" method="POST">
 
-                    <label><b>Mot de passe</b></label>
-                    <input type="password" required="required" placeholder="Mot de passe" name="password" required>
-
-                    <input style="border-radius: 100px;" type="submit" id='submit' value='Se connecter' >
-                    <?php
-                    if(isset($_GET['erreur'])){
-                        $err = $_GET['erreur'];
-                        if($err==1 || $err==2)
-                            echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
-                    }
-                    ?>
-                    <div class="google-btn">
-                          <div class="google-icon-wrapper">
-                            <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
-                          </div>
-                          <p class="btn-text"><b>Sign in with google</b></p>
-                    </div>
-            </form>
+        <div class="logo">
+            <a href=""><img src="../images/LBR Ressources/logoLONGUEURClassic.png"></a>
         </div>
-    </body>
+        <h1>Connexion</h1>
+        <label><b>Email</b>
+            <input type="text" placeholder="Email" name="email" required>
+        </label>
+
+        <label><b>Mot de passe</b>
+            <input type="password" placeholder="Mot de passe" name="password" required>
+        </label>
+
+        <input style="border-radius: 100px;" type="submit" id='submit' value='Se connecter'>
+        <?php
+        if (isset($_GET['erreur'])) {
+            $err = $_GET['erreur'];
+            if ($err == 1 || $err == 2)
+                echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+        }
+        ?>
+        <h4 class="separator"><span>OU</span></h4>
+        <button type="button" class="google-btn">Se connecter avec Google</button>
+    </form>
+</div>
+</body>
 </html>
