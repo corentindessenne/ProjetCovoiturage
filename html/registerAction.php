c10a33848a8c6137155a3675e589b3750d4583e1
Corentin DESSENNE <corentin.dessenne@student.junia.com>
2022-06-21 14:51:48 +0200

    mail confirmation compte + template

--- html/registerAction.php
+++ html/registerAction.php
@@ -8,12 +8,12 @@
 <body>
 
 <?php
-if(isset($_POST["reg_user"]) && $_POST["email"]){
-    include 'Connexion.php';
-    $request = mysqli_query($conn, "SELECT * FROM compte WHERE Email = '".$_POST["email"]."'");
+if (isset($_POST["reg_user"]) && $_POST["email"]) {
+    include('Connexion.php');
+    $request = mysqli_query($conn, "SELECT * FROM compte WHERE Email = '" . $_POST["email"] . "'");
     $row = mysqli_num_rows($request);
 
-    if($row < 1){
+    if ($row < 1) {
         $token = md5($_POST["email"]);
         $password = $_POST["password_1"];
         $password = password_hash($password, PASSWORD_DEFAULT);
@@ -23,36 +23,48 @@ if(isset($_POST["reg_user"]) && $_POST["email"]){
         } else {
             $verifadmin = 0;
         }
-        $request = mysqli_query($conn, "INSERT INTO compte(Nom,Prenom, Email, telephone, motDePasse, isAdmin,DateCreation, lien_verif_mail) VALUES ('" . $_POST["nom"] . "','" . $_POST["prenom"] . "','" . $_POST["email"] . "','" . $tel . "','" . $password . "','" . $verifadmin . "','" . date("Y.m.d") . "', '" . $token . "')");
-        $link = "<a href='localhost/Plateforme covoiturage/html/verify_email.php?key=".$_POST['email']."&token=".$token."'>Clique ici pour vérifier ton compte</a>";
 
+        $path = 'localhost/Plateforme covoiturage/html/verify_email.php?key='. $_POST['email'].'&token='.$token;
+        $request = mysqli_query($conn, "INSERT INTO compte(Nom,Prenom, Email, telephone, motDePasse, isAdmin,DateCreation, lien_verif_mail) VALUES ('" . $_POST["nom"] . "','" . $_POST["prenom"] . "','" . $_POST["email"] . "','" . $tel . "','" . $password . "','" . $verifadmin . "','" . date("Y.m.d") . "', '" . $token . "')");
+        $link = "<a href='localhost/Plateforme covoiturage/html/verify_email.php?key=" . $_POST['email'] . "&token=" . $token . "' target='_blank' style='font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #000000; display: inline-block;'>Confirme ton compte</a>";
+        $lienBrut = "<a href='localhost/Plateforme covoiturage/html/verify_email.php?key=" . $_POST['email'] . "&token=" . $token . "' target='_blank' style='color: #FFA73B;'>".$path."</a>";
 
         $headers = "MIME-Version: 1.0" . "\r\n";
         $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-        $dest = $_POST['email'];
-        $sujet = "Confirme ton compte";
-        $corp = "Clique sur ce lien pour confirmer ton adresse mail : \n".$link;
         $headers .= "From:Les Briques Rouges<cocodsn2@gmail.com>";
 
-        if(mail($dest, $sujet, $corp, $headers)){
-            echo "<script type='text/javascript'>alert('Vérifie ta boîte mail et clique sur le lien de confirmation.');</script>";
-            ?>
-            <script>document.location.href='../html/home.php';</script>
-<?php
+        $dest = $_POST['email'];
+        $sujet = "Merci d'avoir rejoint la plateforme !";
+        $corp = file_get_contents("../mails/confirmation_mail.php");
+
+        $variables = array(
+            "{{Prenom}}" => $_POST['prenom'],
+            "{{Bouton}}" => $link,
+            "{{Lien}}" => $lienBrut
+        );
+
+        foreach ($variables as $key => $value) {
+            $corp = str_replace($key, $value, $corp);
         }
-        else{
-            echo "<script type='text/javascript'>alert('Le mail ne s\'est pas envoyé');</script>";
-            ?>
-            <script>document.location.href='../html/home.php';</script>
-<?php
-        }
+
+    if (mail($dest, $sujet, $corp, $headers)) {
+        echo "<script type='text/javascript'>alert('Vérifie ta boîte mail et clique sur le lien de confirmation.');</script>";
+        ?>
+        <script>document.location.href = '../html/home.php';</script>
+    <?php
     }
     else{
-        echo "<script type='text/javascript'>alert('Oups, il semble que tu aies déjà créé un compte. Vérifie ta boîte mail et confirme ton compte.');</script>";
-        ?>
-        <script>document.location.href='../html/home.php';</script>
-<?php
+    echo "<script type='text/javascript'>alert('Le mail ne s\'est pas envoyé');</script>";
+    ?>
+        <script>document.location.href = '../html/home.php';</script>
+    <?php
+    }
+    }
+    else{
+    echo "<script type='text/javascript'>alert('Oups, il semble que tu aies déjà créé un compte. Vérifie ta boîte mail et confirme ton compte.');</script>";
+    ?>
+        <script>document.location.href = '../html/home.php';</script>
+        <?php
     }
 }
 
