e3f8a049d7c794e86e1a741f2fa7920a573ab127
Corentin DESSENNE <corentin.dessenne@student.junia.com>
2022-06-21 16:34:13 +0200

    mail réinitialisation mot de passe + template

--- html/forget_password_action.php
+++ html/forget_password_action.php
@@ -12,15 +12,16 @@ if(isset($_POST["password-reset"]) && $_POST["email"]){
         $expDate = date("Y-m-d H:i:s", $expFormat);
 
         $update = mysqli_query($conn, "UPDATE compte SET token_reset_password = '$token', expiration_reset_password = '$expDate' WHERE Email = '$email'");
-        $link = "<a href='localhost/Plateforme covoiturage/html/reset_password.php?key=".$email."&token=".$token."'>Clique ici pour réinitialiser ton mot de passe</a>";
+        $link = "<a href='localhost/Plateforme covoiturage/html/reset_password.php?key=".$email."&token=".$token."' target='_blank' style='font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid; display: inline-block;'>Réinitialise ton mot de passe</a>";
 
         $headers = "MIME-Version: 1.0" . "\r\n";
         $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        $headers .= "From:Les Briques Rouges<cocodsn2@gmail.com>";
 
         $dest = $email;
-        $sujet = "Réinitialise ton mot de passe";
-        $corp = "Clique sur ce lien pour réinitialiser ton mot de passe : \n".$link;
-        $headers .= "From:Les Briques Rouges<cocodsn2@gmail.com>";
+        $sujet = "Demande de réinitialisation de ton mot de passe";
+        $corp = file_get_contents("../mails/reset_mot_de_passe.php");
+        $corp = str_replace("{{Bouton}}",$link, $corp);
 
         if(mail($dest, $sujet, $corp, $headers)){
             echo "<script type='text/javascript'>alert('On t\'a envoyé un mail contenant un lien qui te permet de réinitialiser ton mot de passe.');</script>";
